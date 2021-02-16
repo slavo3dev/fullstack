6a618107b2cb6a1b8140e3889d41b79c98692a17
slavo3dev <slavo@slavo3.com>
2021-02-16 06:56:55 -0500

    adding react project

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>ReactLion</p>
+        <p>We are comming soon</p>
+        <a className="App-link" href="https://slavo3.com" target="_blank">
+          Slavo3
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
