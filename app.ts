45ade741ff991a48b84c1f7938d3d2e847e14a1e
slavo3dev <slavo@slavo3.com>
2021-02-16 07:04:17 -0500

    adding port

--- app.ts
+++ app.ts
@@ -1,9 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, "./client/build")));
+
 app.get("/", (req, res) => {
-  res.send("React Lion - WebSite ( React / Node / SQL) - all TS");
+  res.sendFile(path.join(__dirname, "./client/build", "index.html"));
 });
 
 app.listen(port, () => {
