aacd15991bdeffce708bbff982e7af2140560f56
slavo3dev <slavo@slavo3.com>
2021-02-18 08:25:21 -0500

    fixing typescript props

--- client/src/components/HeaderComponent/index.tsx
+++ client/src/components/HeaderComponent/index.tsx
@@ -1,11 +1,34 @@
 import React from "react";
-import { Header, Image } from "semantic-ui-react";
-import logo from "../../assets/logo192.png";
+import { Header, Container, Button, Icon } from "semantic-ui-react";
 
-export default function HeaderComponent() {
+export default function HeaderComponent({ mobile }: { mobile: boolean }) {
   return (
-    <Header as="h2" color="blue">
-      <Image circular src={logo} /> ReactLion
-    </Header>
+    <Container text>
+      <Header
+        as="h1"
+        content="Imagine-a-Company"
+        inverted
+        style={{
+          fontSize: mobile ? "2em" : "4em",
+          fontWeight: "normal",
+          marginBottom: 0,
+          marginTop: mobile ? "1.5em" : "3em",
+        }}
+      />
+      <Header
+        as="h2"
+        content="Do whatever you want when you want to."
+        inverted
+        style={{
+          fontSize: mobile ? "1.5em" : "1.7em",
+          fontWeight: "normal",
+          marginTop: mobile ? "0.5em" : "1.5em",
+        }}
+      />
+      <Button primary size="huge">
+        Get Started
+        <Icon name="arrow right" />
+      </Button>
+    </Container>
   );
 }
